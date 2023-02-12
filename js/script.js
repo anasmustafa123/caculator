4d8ca9e5b9f37cd7954bdffdbb65518a5700be5b
anas mustafa <anasmuostafa@gmail.com>
2023-02-12 09:13:51 +0200

    adjust border and screen layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
- q
+
+//making each operation function
+function add(a,b){
+    return a + b;
+}
+function sub(a,b){
+    return a - b;
+}
+function mul(a,b){
+    return a * b;
+}
+function div(a,b){
+    return a / b;
+}
+
+// operator function returns the result of the operation between a,b
+function operate(operator, a, b){
+    switch (operator){
+        case '+' : return add(a,b); 
+        case '-' : return sub(a,b); 
+        case '*' : return mul(a,b); 
+        case '/' : return div(a,b); 
+        default : throw new ArgumentException
+    }
+}
+/////////////////////
+const numsContainer  = document.querySelector('.numbers-container');
+
+for (let i = 0; i <= 9; i++){
+    const number  = document.createElement('btn');
+    number.id = i; 
+    numsContainer.appendChild(number);  
+}
